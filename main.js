0f096f6f76c7c79b801801815be151430fabd0a3
Đặng Hoàng Giang <hoanggiang.eri.02042006@gmail.com>
2024-09-24 16:46:05 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+alert("Hello");
+alert("jgfgfgfghfgh");
+let ex = prompt("Nhập tên của bạn:");
+let soC = parseInt(prompt("nhap du lieu dau vao: "));
+console.log(ex);
+let number = 100;
+console.log(number);
+let soA = 25;
+let soB = 5;
+
+console.log("Can bac hai cua" + soA + "la:" + soB);
+// console.log("Can bac hai cua" + soA + "la:" + soB);
+console.log(`tong cua hai so ${soB}+${soC}`);
